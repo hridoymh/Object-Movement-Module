31f0b1fb572b08577e92606f7574b3479e53ba09
hridoymh <mhhridoy0c@gmail.com>
2019-10-25 15:57:40 +0800

    first commit

---
+++ ObjectMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public Transform pos1,pos2;
+    Vector3 nextpos;
+    
+    public float speed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        nextpos = pos1.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pos1.position) nextpos = pos2.position;
+        if (transform.position == pos2.position) nextpos = pos1.position;
+
+        transform.position =  Vector3.MoveTowards(transform.position, nextpos, speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        col.gameObject.transform.parent = this.transform;
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        col.gameObject.transform.parent = null;
+    }
+
+}
